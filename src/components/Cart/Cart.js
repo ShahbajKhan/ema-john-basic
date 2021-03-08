d98df5179be6f46448ce0ab28003818c413723ac
ShahbajKhan <shktoky@gmail.com>
2021-03-08 10:42:50 +0600

    added react router, made the cart dynamic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 const Cart = (props) => {
     const cart = props.cart;
     // total product price calculation using reduce
-    const total = cart.reduce((total, prd) => total + prd.price, 0)
+    const total = cart.reduce((total, prd) => total + prd.price*prd.quantity, 0)
     
     // Using for loop total calculation
     // let total = 0;
@@ -24,11 +25,14 @@ const Cart = (props) => {
 
     return (
         <div>
-            <h4>Order summary</h4>
+            <h4 className="fw-bold text-warning">Order summary</h4>
             <p>Items Ordered: {cart.length}</p>
             <p>Product Price: {total.toFixed(2)}</p>
             <p><small>Shipping Price: {shipping}</small></p>
             <h5>Total price: {(shipping + total).toFixed(2)}</h5>
+            {
+                props.children
+            }
         </div>
     );
 };
